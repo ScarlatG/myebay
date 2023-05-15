e86a615a2e0d517581f56c830d5cf0fb1bc42ff5
Gabriel Scarlat <gabrielscarlat36@gmail.com>
2023-05-15 10:23:52 +0200

    Aggiunta e test del login

--- src/main/java/it/prova/myebay/web/controller/HomeController.java
+++ src/main/java/it/prova/myebay/web/controller/HomeController.java
@@ -1,13 +1,23 @@
 package it.prova.myebay.web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import it.prova.myebay.dto.CategoriaDTO;
+import it.prova.myebay.service.CategoriaService;
 
 @Controller
 public class HomeController {
 
-	@GetMapping("/")
-	public String home() {
+	@Autowired
+	private CategoriaService categoriaService;
+
+	@RequestMapping(value = { "/home", "" })
+	public String home(Model model) {
+		model.addAttribute("categorie_list_attribute",
+				CategoriaDTO.createCategoriaDTOListFromModelList(categoriaService.listAll()));
 		return "index";
 	}
 }
